4d6fc30ddf8854b5f804ab01202c39f18e7f2b96
dinesz <dineshksami@gmail.com>
2026-07-16 13:50:19 +0530

    DNAC api call for inventory report fetching
    Added a script to generate authentication token and retrieve network device information from Catalyst Center API, saving the data in a CSV file.

--- Dnac Filez/Dnac_Api_call.py
+++ Dnac Filez/Dnac_Api_call.py
@@ -1,3 +1,18 @@
+# This Script will generate the authentication token for Catalyst Center and then use that token to retrieve network device information from the Catalyst Center API. The retrieved data will be saved in a CSV file.
+
+import csv  # Use when u working on CSV
+import requests
+import json   # Required when u converting the json data
+import pandas as pd  # Use when u beautify the excel
+from openpyxl import Workbook
+
+# Catalyst Center details
+CC_IP = "192.168.1.10"      # Replace with your Catalyst Center IP/FQDN
+USERNAME = "DNACUser"       # Replace with your Catalyst Center Username
+PASSWORD = "CatcPass"       # Replace with your Catalyst Center Password
+
+url = f"https://192.168.1.10/dna/system/api/v1/auth/token"  # Example
+
 url = f"https://{CC_IP}/dna/system/api/vl/auth/token"
 
 Bresponse = requests.post(
